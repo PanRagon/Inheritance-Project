a11b66fdc3c7fd4e5b34318b008f4614ea2b0be8
PanRagon <thebigecko@gmail.com>
2020-01-15 16:01:48 +0100

    Add modifier to ensure inheritance hasn't already been paid out, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Inheritance Project
- Simple Inheritance smart contract to allow funds be transferred to beneficiary over a set amount of time
+ Simple Inheritance smart contract to allow funds be transferred to a beneficiary over a set amount of time, on a weekly basis.
+
+ The current build has not been tested and should not be deployed in it's current state. Do so at your own risk.
+
+
+ #TODO
+ * Implement unit tests and deploy on Ropsten test network.
+ * Allow contract owner to specify what time units he wants the contract to release funds in (days, months, years).
+ * Allow for multiple beneficiary addresses with different percentages of total wealth to inherit.
+
+ #License
+ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -5,70 +5,89 @@ pragma solidity >=0.4.21 <0.7.0;
  contract Inheritance {
     using SafeMath for uint256;
 
-     mapping (address => uint) balance;
+    mapping (address => uint) balance;
     address owner;
     uint256 timeline;
     address beneficiary;
     uint256 value;
     bool started = false;
+    bool ended = false;
     uint256 weeklyPayout;
     uint256 valueToWithdraw;
     uint256 contractStart;
     uint256 lastTimeStamp;
+    uint256 startDate;
 
-     modifier _ownerOnly() {
+    modifier _ownerOnly() {
         require(msg.sender == owner, "Sender is not the owner of this contract");
         _;
     }
 
-     modifier _beneficiaryOnly() {
+    modifier _beneficiaryOnly() {
         require(msg.sender == beneficiary, "Sender is not a beneficiary of this contract");
         _;
     }
 
-     modifier _notStarted() {
+    modifier _notStarted() {
         require(started == false, "The inheritance has already started");
         _;
     }
 
-     constructor() public payable {
+    modifier _notEnded() {
+        require(ended == false, "The inheritance has already ended");
+        _;
+    }
+
+    constructor() public payable {
         owner = msg.sender;
         value = msg.value;
     }
 
-     function addFunds() public _ownerOnly _notStarted payable {
+    function addFunds() public _ownerOnly _notStarted payable {
         value += msg.value;
     }
 
-     function setTimeline(uint lengthInWeeks) public {
+    function setTimeline(uint lengthInWeeks) public  _notStarted {
         timeline = lengthInWeeks * 1 weeks;
     }
 
-     function setBeneficiary(address _beneficiary) public _ownerOnly {
+    function setBeneficiary(address _beneficiary) public _notStarted _ownerOnly {
         beneficiary = _beneficiary;
     }
 
-     function balanceOfContract() public _ownerOnly _beneficiaryOnly view returns (uint) {
+    function setStartDate(uint256 _startDate) public _notStarted _ownerOnly {
+        startDate = _startDate;
+    }
+
+    function getBalanceOfContract() public _notEnded _ownerOnly _beneficiaryOnly view returns (uint256) {
         return value;
     }
 
-     function calculateWeeklyPayout() internal {
+    function getBalanceAvailableForWithdrawal() public _notEnded _beneficiaryOnly view returns (uint256) {
+        return valueToWithdraw;
+    }
+
+    function calculateWeeklyPayout() internal {
         weeklyPayout = SafeMath.div(value, timeline);
     }
 
-     function startInheritance() public _ownerOnly {
+    function startInheritance() public _notStarted _ownerOnly {
         started = true;
         calculateWeeklyPayout();
         lastTimeStamp = block.timestamp;
     }
 
-     function withdraw(uint256 amount) public _beneficiaryOnly payable {
+    function withdraw(uint256 amount) public _beneficiaryOnly _notEnded payable {
+        updateValueToWithdraw();
         require(amount < valueToWithdraw, "There isn't enough Ether available to withdraw yet");
+        SafeMath.sub(value, amount);
+        SafeMath.sub(valueToWithdraw, amount);
         msg.sender.transfer(amount);
     }
 
-    function updateValueToWithdraw() public {
+    function updateValueToWithdraw() public _notEnded {
         bool updated = false;
+        endInheritanceContract;
         while(updated == false) {
             if(block.timestamp - 1 weeks >= lastTimeStamp) {
                 valueToWithdraw += weeklyPayout;
@@ -77,4 +96,10 @@ pragma solidity >=0.4.21 <0.7.0;
         }
     }
 
- }
+    function endInheritanceContract() internal {
+        if(value == 0) {
+            ended = true;
+        }
+    }
+
+}
